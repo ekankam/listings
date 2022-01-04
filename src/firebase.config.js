6d189780ae6127ea4b084468ba7ffc1449fa9e32
ekankam <eb.kankam26@gmail.com>
2022-01-03 19:07:50 -0600

    first commit

---
+++ src/firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBu1TB_yGE--hItiqv-CN1tR8PrCSbR8xs",
+  authDomain: "property-listings-app-a154f.firebaseapp.com",
+  projectId: "property-listings-app-a154f",
+  storageBucket: "property-listings-app-a154f.appspot.com",
+  messagingSenderId: "486078640648",
+  appId: "1:486078640648:web:cefd6538b42f19f11b97bd"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
