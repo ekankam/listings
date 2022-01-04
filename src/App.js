bee203fa2e2cc3df2d56c2925d4337f1876ade68
ekankam <eb.kankam26@gmail.com>
2022-01-03 21:11:29 -0600

    Created a config file for firebase settings. Installed firebase. Imported asset files. Created pages folder and pages. Set up routing for pages.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import {
+    Explore,
+    Offers,
+    Profile,
+    SignIn,
+    SignUp,
+    ForgetPassword,
+} from './pages'
+
+const App = () => {
+    return (
+        <>
+            <Router>
+                <Routes>
+                    <Route path="/" element={<Explore />} />
+                    <Route path="/offers" element={<Offers />} />
+                    <Route path="/profile" element={<SignIn />} />
+                    <Route path="/sign-in" element={<SignIn />} />
+                    <Route path="/sign-up" element={<SignUp />} />
+                    <Route
+                        path="/forget-password"
+                        element={<ForgetPassword />}
+                    />
+                </Routes>
+            </Router>
+        </>
+    )
 }
 
-export default App;
+export default App
